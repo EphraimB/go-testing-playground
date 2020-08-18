e23caf2f624e06c90eec90413aa1d8e490ac3d67
Ephraim Becker <ephraimbecker@Ephraims-MBP.lan>
2020-08-18 11:24:16 -0400

    Moved the code into the go-testing-tutorials folder.

--- go-testing-tutorial/main.go
+++ go-testing-tutorial/main.go
@@ -10,6 +10,24 @@ func Calculate(x int) (result int) {
 	return result
 }
 
+func add(x int, y int) (result int) {
+	result = x + y
+
+	return result
+}
+
+func subtract(x int, y int) (result int) {
+	result = x - y
+
+	return result
+}
+
+func multiply(x int, y int) (result int) {
+	result = x * y
+
+	return result
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
