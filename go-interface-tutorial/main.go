49ee3ec44b08872fc7b42962022dc504ea096163
EphraimB <=>
2020-09-03 14:58:37 -0400

    Got a result back from postgres.

--- go-interface-tutorial/main.go
+++ go-interface-tutorial/main.go
@@ -14,8 +14,8 @@ import (
 // fine for an interface to describe multiple methods, and that it should
 // describe input parameter types as well as return value types.
 type ShopModel interface {
-	CountCustomers(time.Time) (int, error)
-	CountSales(time.Time) (int, error)
+	//CountCustomers(time.Time) (int, error)
+	//CountSales(time.Time) (int, error)
 	CreateBooks() (int, error)
 }
 
@@ -52,31 +52,41 @@ func main() {
 	defer db.Close()
 
 	shopDB := &ShopDB{db}
-	sr, err := calculateSalesRate(shopDB)
+	//sr, err := calculateSalesRate(shopDB)
+	sr, err := createBooks(shopDB)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf(sr)
 }
 
+func createBooks(sm ShopModel) (string, error) {
+	books, err := sm.CreateBooks()
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%.2f", books), nil
+}
+
 // Swap this to use the ShopModel interface type as the parameter, instead of the
 // concrete *ShopDB type.
-func calculateSalesRate(sm ShopModel) (string, error) {
-	since := time.Now().Add(-24 * time.Hour)
+// func calculateSalesRate(sm ShopModel) (string, error) {
+// 	since := time.Now().Add(-24 * time.Hour)
 
-	sales, err := sm.CountSales(since)
-	if err != nil {
-		return "", err
-	}
+// 	sales, err := sm.CountSales(since)
+// 	if err != nil {
+// 		return "", err
+// 	}
 
-	customers, err := sm.CountCustomers(since)
-	if err != nil {
-		return "", err
-	}
+// 	customers, err := sm.CountCustomers(since)
+// 	if err != nil {
+// 		return "", err
+// 	}
 
-	rate := float64(sales) / float64(customers)
-	return fmt.Sprintf("%.2f", rate), nil
-}
+// 	rate := float64(sales) / float64(customers)
+// 	return fmt.Sprintf("%.2f", rate), nil
+// }
 
 type Repository interface {
 	search(query string) []string
