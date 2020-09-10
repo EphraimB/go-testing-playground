c65a549fd63aa4f833d05a0ad17a8e2592a91437
EphraimB <=>
2020-09-10 10:39:34 -0400

    Trying to implement a test that mocks the postgres queries.

--- go-interface-tutorial/main_test.go
+++ go-interface-tutorial/main_test.go
@@ -1,34 +1,52 @@
 // File: main_test.go
 package main
 
-import (
-	"testing"
-	"time"
-)
+import "testing"
 
 type MockShopDB struct{}
 
-func (m *MockShopDB) CountCustomers(_ time.Time) (int, error) {
-	return 1000, nil
+// func (m *MockShopDB) CountCustomers(_ time.Time) (int, error) {
+// 	return 1000, nil
+// }
+
+// func (m *MockShopDB) CountSales(_ time.Time) (int, error) {
+// 	return 333, nil
+// }
+
+func (m *MockShopDB) CreateBooks() (bool, error) {
+	return true, nil
 }
 
-func (m *MockShopDB) CountSales(_ time.Time) (int, error) {
-	return 333, nil
-}
-
-func TestCalculateSalesRate(t *testing.T) {
+func TestCreateBooks(t *testing.T) {
 	// Initialize the mock.
 	m := &MockShopDB{}
 	// Pass the mock to the calculateSalesRate() function.
-	sr, err := calculateSalesRate(m)
+	sr, err := createBooks(m)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// Check that the return value is as expected, based on the mocked
 	// inputs.
-	exp := "0.33"
+	exp := ""
 	if sr != exp {
 		t.Fatalf("got %v; expected %v", sr, exp)
 	}
 }
+
+// func TestCalculateSalesRate(t *testing.T) {
+// 	// Initialize the mock.
+// 	m := &MockShopDB{}
+// 	// Pass the mock to the calculateSalesRate() function.
+// 	sr, err := calculateSalesRate(m)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	// Check that the return value is as expected, based on the mocked
+// 	// inputs.
+// 	exp := "0.33"
+// 	if sr != exp {
+// 		t.Fatalf("got %v; expected %v", sr, exp)
+// 	}
+// }
